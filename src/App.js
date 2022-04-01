e90f32f9b173328602f9886e960e856b837c1bee
lauragomez04 <lauragomezb0904@gmail.com>
2022-04-01 12:11:32 +0100

    Project Set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="btn btn-primary">Hello</div>
+      Hello World
+    </div>
+  );
+}
+
+export default App;
