bab6047023b2845a6d8a0accf15fe72a1da9a305
lauragomez04 <lauragomezb0904@gmail.com>
2022-04-10 19:35:55 +0100

    finished styling about section

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,15 +5,17 @@ export default function AboutMe() {
   return (
     <section className="about-me">
       <div className="row">
-        <div className="col d-flex align-items-center image-email">
-          <a className="email-link" href="mailto:lauragomez.codes@gmail.com">
-            lauragomez.codes@gmail.com
-          </a>
-          <img
-            className="img-fluid about-img"
-            src="./images/about-me-img.jpg"
-            alt="about me"
-          />
+        <div className="col d-none d-md-block">
+          <div className="d-flex align-items-center image-email">
+            <a className="email-link" href="mailto:lauragomez.codes@gmail.com">
+              lauragomez.codes@gmail.com
+            </a>
+            <img
+              className="img-fluid about-img"
+              src="./images/about-me-img.jpg"
+              alt="about me"
+            />
+          </div>
         </div>
         <div className="col">
           <h2 className="text-center about-title">About Me</h2>
