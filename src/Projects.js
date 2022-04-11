57a77393576b986134f71ed9cf2bd455c2567e7f
lauragomez04 <lauragomezb0904@gmail.com>
2022-04-11 12:25:57 +0100

    Added content and styles to projects component

--- src/Projects.js
+++ src/Projects.js
@@ -4,10 +4,16 @@ import "./Projects.css";
 export default function Projects() {
   return (
     <div className="Projects">
-      <h2 className="text-center title projects-title">Projects</h2>
+      <h2 className="text-center title projects-heading">Projects</h2>
       <div className="row">
         <div className="col project-1">
-          <h3 className="text-center">01</h3>
+          <h3 className="text-center number">01</h3>
+          <img
+            className="img-fluid"
+            src="./images/weather-app-project.jpg"
+            alt="weather app project"
+          />
+          <h3 className="text-center project-title">Weather App</h3>
         </div>
         <div className="col project-2">
           <h3 className="text-center">02</h3>
